6c4cc39147fc2a459862bf5b013aacf136b03c1e
Maksym Marko <maximus21@mail.ru>
2016-03-09 11:08:22 +0200

    Styling scroll using Jscrollpane

--- js/select.js
+++ js/select.js
@@ -1,10 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function(){	
 	
 	$('.select-par li').click(function(){
 		var obj_jq = $(this);
 		var object_li = this;
 		object_li.val = obj_jq.text();
 		object_li.index = obj_jq.index();
-		alert(object_li.val);
+		$('.select-conclusion').text(object_li.val);
+		$('.par-input').val(object_li.val);
 	});
 })
